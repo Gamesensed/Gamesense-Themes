f5a96e15380605e42e25410ee058fdde00ceda10
nex <headstruck@protonmail.ch>
2024-02-27 13:14:59 +0000

    i really need to automate this somehow

--- loader.user.js
+++ loader.user.js
@@ -2,7 +2,7 @@
 // @name                    Custom Gamesense Themes
 // @namespace               https://gamesense.pub/forums/*
 // @author                  Nexxed & AnonVodka
-// @version                 2.0.5-beta
+// @version                 2.0.6-beta
 
 // @supportURL              https://github.com/Gamesensed/Gamesense-Themes
 // @updateURL               https://gsext.nex.wtf/loader.user.js
